bef4679a667712d7c251d5450a83dc9a5483bfb0
Kevin Ng <80858693+kn2118@users.noreply.github.com>
2022-07-08 18:56:29 -0400

    autofill display without styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React from "react";
+import {useState} from "react";
 import PlacesAutocomplete, {
   geocodeByAddress, 
   geoLatLng 
 } from "react-places-autocomplete";
 
 export default function App() {
-  const [address, setAddress] = React.useState("")
-  const handleSelect = async value => {}; 
+  const [address, setAddress] = useState("")
+  const handleSelect = async value => {}; //recieves of address of selection 
 
   return (
     <div>
-      <PlacesAutocomplete
-        value = {address}
-        onChange = {setAddress}
-        onSelect = {handleSelect}
-      >
-        {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (<div>
-            <input {...getInputProps({placeholder: "Type Address"})}></input>
-            <div>
-              {loading ? <div>...loading</div> : null}
-              {suggestions.map( () => {
-                
+      <PlacesAutocomplete //needs a render prop function 
+        value = {address} //current value of whatever user is typing into input
+        onChange = {setAddress} //called whenever input changes
+        onSelect = {handleSelect} //called when user selects one of the dropdown options 
+      > 
+       {/*this render prop function needs a couple of props*/}
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input {...getInputProps({placeholder: "Type Address"})}></input>  {/*renders input to screen*/}
+
+            <div>  {/*div that displays suggestions */}
+              {loading ? <div>...loading</div> : null} 
+              { suggestions.map( suggestion => { 
+                  return (
+                    <div>
+                      {suggestion.description}
+                    </div>
+                  )
               })}
             </div>
-          </div>)}
+
+          </div>
+          )}
       </PlacesAutocomplete>
     </div>
   )
